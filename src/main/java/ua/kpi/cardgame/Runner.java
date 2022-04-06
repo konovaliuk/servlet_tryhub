a7eb6935d27d392391c285c696b00c4e4ffcb2a0
Razumovska Yelyzaveta <miyatna@gmail.com>
2022-04-06 18:30:25 +0300

    Added DAO interfaces

--- src/main/java/ua/kpi/cardgame/Runner.java
+++ src/main/java/ua/kpi/cardgame/Runner.java
@@ -1,9 +1,15 @@
 package ua.kpi.cardgame;
 
+import ua.kpi.cardgame.dao.impl.PSQLEventDAO;
+import ua.kpi.cardgame.dao.interfaces.EventDAO;
+
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 
 public class Runner {
     public static void main(String[] args) throws SQLException {
-        
+        EventDAO eventDAO = new PSQLEventDAO();
+
+        System.out.println(eventDAO.getEventById(2));
     }
 }
